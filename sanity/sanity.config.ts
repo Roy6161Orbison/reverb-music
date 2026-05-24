03a09323fa1c23f09f6645714208dcf4cb41a871
Yuto Hiroi <yutohiroi@gmail.com>
2026-05-24 18:58:59 +0900

    諸々の修正 Sanityでstudioが表示された

--- sanity/sanity.config.ts
+++ sanity/sanity.config.ts
@@ -1,15 +1,19 @@
 import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
 import {visionTool} from '@sanity/vision'
+import {schemaTypes} from './schemaTypes'
+
+const projectId = '941ja3ai'
+const dataset = 'production'
 
 export default defineConfig({
   name: 'reverb-music',
   title: 'Reverb / 残響',
-  projectId: '53mlj72m',
-  dataset: 'production',
+  projectId: projectId,
+  dataset: dataset,
   basePath: '/studio',
   plugins: [structureTool(), visionTool()],
   schema: {
-    types: [],
+    types: schemaTypes,
   },
 })
