6d9568e448198728cc5d9104b8afd59a0eaf715e
Yuto Hiroi <yutohiroi@gmail.com>
2026-05-24 20:23:48 +0900

    fix sanity.config.ts 環境変数

--- sanity/sanity.config.ts
+++ sanity/sanity.config.ts
@@ -1,19 +1,16 @@
 import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
 import {visionTool} from '@sanity/vision'
-import {schemaTypes} from './schemaTypes'
-
-const projectId = '941ja3ai'
-const dataset = 'production'
+import {article} from './schemaTypes/article'
 
 export default defineConfig({
   name: 'reverb-music',
   title: 'Reverb / 残響',
-  projectId: projectId,
-  dataset: dataset,
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || '941ja3ai',
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
   basePath: '/studio',
   plugins: [structureTool(), visionTool()],
   schema: {
-    types: schemaTypes,
+    types: [article],
   },
 })
