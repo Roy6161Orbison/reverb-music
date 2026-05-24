7328a7d04cab0b1d1b92e30412ecbc6d9117140a
Yuto Hiroi <yutohiroi@gmail.com>
2026-05-24 17:15:21 +0900

    sanityの設定

---
+++ sanity/schemaTypes/article.ts
@@ -0,0 +1,71 @@
+import {defineField, defineType} from 'sanity'
+
+export const article = defineType({
+  name: 'article',
+  title: 'Article',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'title',
+      },
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      name: 'excerpt',
+      title: 'Excerpt',
+      type: 'text',
+    }),
+    defineField({
+      name: 'body',
+      title: 'Body',
+      type: 'array',
+      of: [{type: 'block'}],
+    }),
+    defineField({
+      name: 'publishedAt',
+      title: 'Published At',
+      type: 'datetime',
+      initialValue: () => new Date().toISOString(),
+    }),
+    defineField({
+      name: 'type',
+      title: 'Type',
+      type: 'string',
+      options: {
+        list: [
+          {title: 'Review', value: 'review'},
+          {title: 'Feature', value: 'feature'},
+          {title: 'Interview', value: 'interview'},
+          {title: 'News', value: 'news'},
+        ],
+      },
+    }),
+    defineField({
+      name: 'artist',
+      title: 'Artist',
+      type: 'string',
+    }),
+    defineField({
+      name: 'score',
+      title: 'Score',
+      type: 'object',
+      fields: [
+        defineField({
+          name: 'overall',
+          title: 'Overall',
+          type: 'number',
+        }),
+      ],
+    }),
+  ],
+})
